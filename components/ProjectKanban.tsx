47da27e4e906d48c6c439312818edd28859250f2
euyuriadriel <60609385+xCONEx@users.noreply.github.com>
2025-06-08 22:54:19 -0300

    Delete components directory

--- components/ProjectKanban.tsx
+++
@@ -1,9 +0,0 @@
-
-// Substituir completamente por ImprovedKanban
-import ImprovedKanban from './ImprovedKanban';
-
-const ProjectKanban = () => {
-  return <ImprovedKanban />;
-};
-
-export default ProjectKanban;
--- components/TeamManagement.tsx
+++
@@ -1,9 +0,0 @@
-
-// Substituir completamente por CompanyDashboard
-import CompanyDashboard from './CompanyDashboard';
-
-const TeamManagement = () => {
-  return <CompanyDashboard />;
-};
-
-export default TeamManagement;
