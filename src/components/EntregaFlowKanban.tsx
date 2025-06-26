dea5ea96789d1b3d0bc119107507d469d54eaaca
euyuriadriel <60609385+xCONEx@users.noreply.github.com>
2025-06-26 11:42:56 -0300

    Update EntregaFlowKanban.tsx

--- src/components/EntregaFlowKanban.tsx
+++ src/components/EntregaFlowKanban.tsx
@@ -373,8 +373,6 @@ const EntregaFlowKanban = () => {
           <Plus className="h-4 w-4 mr-2" />
           Novo Projeto
         </Button>
-            {/* Context Selector */}
-            <ContextSelector />
         
       </div>
 
