dcf5970ada78465e9db35c8d49fe9be8dd27c1ba
euyuriadriel <60609385+xCONEx@users.noreply.github.com>
2025-06-29 15:29:49 -0300

    Update MainApp.tsx

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -9,7 +9,6 @@ import Settings from './Settings';
 import UserProfile from './UserProfile';
 import AdminPanel from './AdminPanel';
 import TeamManagement from './TeamManagement';
-import ImprovedKanban from './ImprovedKanban';
 import SubscriptionPlans from './SubscriptionPlans';
 import FinancialManagement from './financial/FinancialManagement';
 import ClientsManagement from './clients/ClientsManagement';
@@ -31,8 +30,6 @@ const MainApp = () => {
         return <Dashboard />;
       case 'calculator':
         return <PricingCalculator />;
-      case 'kanban':
-        return <ImprovedKanban />;
       case 'financial':
         return <FinancialManagement />;
       case 'clients':
