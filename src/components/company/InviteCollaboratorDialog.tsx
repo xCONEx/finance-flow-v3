7b0548de04d1b3601a6c4747e6c278053731e5ae
euyuriadriel <60609385+xCONEx@users.noreply.github.com>
2025-06-29 22:23:45 -0300

    Update InviteCollaboratorDialog.tsx

--- src/components/company/InviteCollaboratorDialog.tsx
+++ src/components/company/InviteCollaboratorDialog.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
