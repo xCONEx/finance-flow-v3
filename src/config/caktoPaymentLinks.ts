bab323515020d7651a3fdf1229facc4d71773924
euyuriadriel <60609385+xCONEx@users.noreply.github.com>
2025-07-15 18:33:10 -0300

    Create caktoPaymentLinks.ts

---
+++ src/config/caktoPaymentLinks.ts
@@ -0,0 +1,9 @@
+// Centralização dos links de pagamento Cakto
+// Altere os links aqui caso haja mudanças futuras
+
+export const caktoPaymentLinks = {
+  basic: 'https://pay.cakto.com.br/rhmtk8f',
+  premium: 'https://pay.cakto.com.br/35mnrur',
+  enterprise: 'https://pay.cakto.com.br/36cyc6r',
+  enterpriseAnnual: 'https://pay.cakto.com.br/6buio5k',
+}; 
